058c00dd5b5494d5ed4a9c311c898ab2ae302fad
Eric Magalhães <ericovis@gmail.com>
2018-02-22 11:31:57 -0300

    removed static files dir

--- api/pizza_time/settings.py
+++ api/pizza_time/settings.py
@@ -133,10 +133,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 if os.environ.get('DJANGO_ENV', '') == 'prod':
     from pizza_time.prod import *
